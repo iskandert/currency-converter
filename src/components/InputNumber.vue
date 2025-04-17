eabd4183fd9310f86d5c39cb01c2d47d8eb6659c
iskandert <alex.tol1998@gmail.com>
2025-04-17 12:22:34 +0300

    Fix decimal separator

--- src/components/InputNumber.vue
+++ src/components/InputNumber.vue
@@ -6,12 +6,12 @@
         hide-details
         :precision="props.precision"
         @focus="$emit('input:focus', $event)"
-        @blur="$emit('input:blur', $event)"
+        @blur="onBlur"
         ></v-text-field>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps({
     value: {
@@ -26,14 +26,27 @@ const props = defineProps({
 
 const emit = defineEmits(['update:value', 'input:focus', 'input:blur'])
 
+const lastSeparator = ref('')
+
 const inputValue = computed({
     get() {
-        return props.value.toString()
+        return props.value.toString() + lastSeparator.value
     },
     set(value: string) {
+        const lastChar = value.slice(-1)
+        if (lastChar === '.' || lastChar === ',') {
+            lastSeparator.value = lastChar
+        } else {
+            lastSeparator.value = ''
+        }
         const parsedValue = parseFloat(value)
         const numberValue = isNaN(parsedValue) ? 0 : parsedValue
         emit('update:value', numberValue)
     },
 })
+
+const onBlur = (event: Event) => {
+    lastSeparator.value = ''
+    emit('input:blur', event)
+}
 </script>
